cd91d0ff937c43fb88319ed15a0a92e31ba87a0d
Kyle Chadha <kyle.chadha@buzzfeed.com>
2014-11-28 14:06:50 -0500

    Update bower library

--- app/routes.js
+++ app/routes.js
@@ -1,23 +1,7 @@
-var Nerd = require('./models/nerd');
-
 module.exports = function(app) {
 
   // Server Routes
   // ----------------------------------------------
-
-  app.get('/api/nerds', function(req, res) {
-    Nerd.find(function(err, nerds) {
-
-      if (err) {
-        res.send(err);
-      }
-
-      res.json(nerds); // return all nerds in JSON format
-    });
-  });
-
-  // app.post
-  // app.delete
 
   
   // Front End Routes
@@ -28,17 +12,3 @@ module.exports = function(app) {
   });
 
 };
-
-
-// Route Middleware
-function isLoggedIn(req, res, next) {
-
-  // If authenticated, proceed
-  if (req.isAuthenticated()) {
-    return next();
-  }
-
-  // If not, redirect them to login
-  res.redirect('/login');
-
-}
--- server.js
+++ server.js
@@ -9,53 +9,23 @@ var express  = require('express');
 var app      = express();
 var path     = require('path');
 var port     = process.env.PORT || 8080;
-
 var morgan       = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser   = require('body-parser');
-var session      = require('express-session');
-
-var mongoose = require('mongoose');
-var passport = require('passport');
-var flash    = require('connect-flash');
-
-
-// Configuration
-// ----------------------------------------------
-var configDB = require('./config/database');
-// mongoose.connect(db.url); 
-// require('./config/passport')(passport);  // pass passport for configuration
 
 
 // Express Set Up
 // ----------------------------------------------
-app.use(morgan('dev'));         // log every request to the console
-app.use(cookieParser());        // read cookies (needed for auth)
-app.use(bodyParser());          // get information from html forms
+app.use(morgan('dev'));
+app.use(bodyParser());
 app.use(express.static(__dirname + '/public'));
-// app.set('views', __dirname + '/public/views');
-// app.use(express.static('public'));
-
-
-// Authentication
-// ----------------------------------------------
-app.use(session({ secret: 'tusmadressonenmispantalones' }));     // session secret -- guessing we should hide this at some point?
-app.use(passport.initialize());                                  // initialize passport
-app.use(passport.session());                                     // persistent login sessions
-app.use(flash());                                                // use connect-flash for flash messages stored in session
 
 
 // Routes
 // ----------------------------------------------
-require('./app/routes.js')(app, passport);  // load our routes and pass in our app and fully configured passport
+require('./app/routes.js')(app);
 
 
 // Server
 // ----------------------------------------------
 app.listen(port);               
 console.log('What happens on port ' + port + ', stays on port ' + port + '.');
-
-
-// Exports
-// ----------------------------------------------
-exports = module.exports = app;
